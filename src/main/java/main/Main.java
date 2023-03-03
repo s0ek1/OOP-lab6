77b25c9e148f789a96f44fc4416f48cb3d6cfce8
s0ek1 <kostya.dank47@gmail.com>
2023-03-03 19:15:14 +0200

    sout

--- src/main/java/main/Main.java
+++ src/main/java/main/Main.java
@@ -1,2 +1,12 @@
-package main;public class Main {
+package main;
+
+public class Main {
+    public static void main(String[] args) {
+        Main main = new Main();
+        main.run();
+    }
+
+    private void run() {
+        System.out.println(1);
+    }
 }
